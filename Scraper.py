2c93494ed2afab7db23372712d04340708d9de8b
friesalafrancais <115602464+friesalafrancais@users.noreply.github.com>
2022-10-11 19:47:34 -0700

    Create Scraper.py

---
+++ Scraper.py
@@ -0,0 +1,31 @@
+# import required libraries
+import requests
+import re
+import csv
+
+import row as row
+from bs4 import BeautifulSoup
+
+url = input("Please enter the URL (ex. http://www.portlandoregon.gov/bhr/81387): ")
+emailsList = []
+
+# Requests the page and then soupifies it
+res = requests.get(url)
+soup = BeautifulSoup(res.content, 'html.parser')
+
+# Use regex to find emails and then add it to the emailsList list
+actualEmails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", soup.text, re.I))
+emailsList.append(actualEmails)
+
+print(*actualEmails, sep = "\n")
+
+with open('output.txt', 'w') as f:
+    for line in actualEmails:
+        f.writelines(line)
+        f.writelines('\n')
+
+
+with open('output.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for i in actualEmails:
+        writer.writerows([[i]])
